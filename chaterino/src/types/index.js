bd8238725339a10671ef9f531ae27837c107b06f
Unknown <jua16073@uvg.edu.gt>
2018-10-22 19:29:45 -0600

    types, actions

---
+++ chaterino/src/types/index.js
@@ -0,0 +1,5 @@
+export const USER_CREATED = "USER_CREATED";
+export const USER_VERIFIED = "USER_VERIFIED";
+export const CHAT_CREATED = "CHAT_CREATED";
+export const CHAT_JOINED = "CHAT_JOINED";
+export const COMMENTED = "COMMENTED";
