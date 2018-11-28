19474bf96dea11b4ad6e5279332eefa00e870a6b
Unknown <jua16073@uvg.edu.gt>
2018-11-28 13:19:49 -0600

    Autenticacion requerida

--- chaterino/src/components/chaterinoApp/index.jsx
+++ chaterino/src/components/chaterinoApp/index.jsx
@@ -2,31 +2,41 @@ import React, {Fragment} from 'react';
 import Form from '../Principal/Form';
 import Login from '../Inicio/LogIn';
 import Registrar from '../Inicio/Register';
+import {connect} from 'react-redux';
 import ChatList from '../Principal/ChatsList';
 import Chat from '../VistaChat/Chat';
 import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+
+
+import * as selectors from '../../reducers';
 
 import './app.css';
 import Home from '../Principal/Home';
 
 
-const chaterinoApp = () => (
+const chaterinoApp = ({auth}) => (
   <Fragment>
     <div className="app">
       <Route exact path="/" component={Login}/>
       <Route path = "/register" component={Registrar}/>
       <Route path = "/chats" exact = {true} component= {Home}/>
       <Route path = "/chats/:id" component={Chat}/>
+      <Route path = "/chats" exact = {true} render = {() => (
+        auth != null ? (<Home/>) : (<Redirect to= "/"/>)
+      )} />  
     </div>
     
   </Fragment>
 );
 /*
-<Route path = "/chats" exact = {true} render = {() => (
-        this.state.users.user != null ? (<Home/>) : (<Redirect to= "/"/>)
-      )} />  
+
 */
 
 
 
-export default chaterinoApp;
+export default connect(
+  state => ({
+    ...selectors.getToken(state),
+  }),
+  undefined
+)(chaterinoApp);
